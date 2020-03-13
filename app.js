e60e4b79795a88b24cd95ee39116cc1cc57e4f5d
v3ct0r22 <rekinvector@gmail.com>
2020-03-13 13:13:55 +0000

    Add README file...

--- app.js
+++ app.js
@@ -228,11 +228,16 @@ var UIController = (function() {
             if(type === 'inc') {
                 element = elements.incomesPanel;
 
-                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div>\
+                <div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete">\
+                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             } else if (type === 'exp') {
                 element = elements.expensesPanel;
 
-                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div>\
+                <div class="right clearfix"><div class="item__value">%value%</div>\
+                <div class="item__percentage">21%</div><div class="item__delete">\
+                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
 
             // Replace the placeholder text by the actual data
